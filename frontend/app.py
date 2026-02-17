873235104500fd4798955520ad79fa6ca388f24f
Likith K <likith.karigowda99455@gmail.com>
2026-02-17 13:04:02 +0530

    Added frontend requirements

--- frontend/app.py
+++ frontend/app.py
@@ -1,18 +1,13 @@
 import streamlit as st
 import requests
 
-st.set_page_config(page_title="Manufacturing Predictor", layout="centered")
+st.title("Manufacturing Output Predictor")
 
-st.title("🏭 Manufacturing Output Predictor")
-
-st.write("Enter values and click Predict")
-
-# Inputs
-Injection_Temperature = st.number_input("Injection Temperature", value=0.0)
-Injection_Pressure = st.number_input("Injection Pressure", value=0.0)
-Cycle_Time = st.number_input("Cycle Time", value=0.0)
-Cooling_Time = st.number_input("Cooling Time", value=0.0)
-Machine_Age = st.number_input("Machine Age", value=0.0)
+Injection_Temperature = st.number_input("Injection Temperature")
+Injection_Pressure = st.number_input("Injection Pressure")
+Cycle_Time = st.number_input("Cycle Time")
+Cooling_Time = st.number_input("Cooling Time")
+Machine_Age = st.number_input("Machine Age")
 
 if st.button("Predict"):
     data = {
@@ -24,8 +19,13 @@ if st.button("Predict"):
     }
 
     try:
-response = requests.post("https://manufacturing-output-predictor.onrender.com/predict", json=data)
+        response = requests.post(
+            "https://manufacturing-output-predictor.onrender.com/predict",
+            json=data
+        )
+
         result = response.json()
         st.success(f"Predicted Parts Per Hour: {result['Predicted_Parts_Per_Hour']}")
+
     except:
-        st.error("Backend not running")
+        st.error("Backend is not responding")
